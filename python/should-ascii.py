40e641256ee19fdbee569f9981f82da814a9cb1b
Laurent Senta <laurent.senta@gmail.com>
2012-05-21 23:30:16 +0200

    should-ascii: get the maximal frequency

--- python/should-ascii.py
+++ python/should-ascii.py
@@ -4,13 +4,15 @@ import sys
 
 def main(args):
     used = []
+    m = 0
     with open(args[1], "r") as f:
         for line in f:
             word, freq = line.split()
             map(lambda x: x not in used and used.append(x), word)
+            m = max(int(freq), m)
     print sorted(used)
     print len(used)
-
+    print "max freq:", m
 
 if __name__ == "__main__":
     main(sys.argv)
