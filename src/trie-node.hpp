86e118856e6b3f68d4e7c04dc84957fbb92d8ac3
Laurent Senta <laurent.senta@gmail.com>
2012-05-21 16:16:25 +0200

    trie-node: structure organization's first draft

--- src/trie-node.hpp
+++ src/trie-node.hpp
@@ -1,1 +1,26 @@
+#ifndef TRIENODE_HPP
+#define TRIENODE_HPP
 
+namespace mitsake {
+
+    struct s_TrieNode {
+        unsigned int frequency;
+        unsigned int child_count;
+    } __attribute__((packed));
+
+    struct s_TrieLink {
+        unsigned int letter   : 7;
+        unsigned int is_final : 1;
+    } __attribute__((packed));
+
+    typedef struct s_TrieNode TrieNode;
+    typedef struct s_TrieLink TrieLink;
+    typedef unsigned int NodeOffset;
+
+    union u_NodeInfo {
+        NodeOffset offset;
+        TrieLink   link;
+    } NodeInfo;
+}
+
+#endif
