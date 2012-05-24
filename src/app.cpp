8ea93df2f214b606cdf2ce919b009072d10a106d
Laurent Senta <laurent.senta@gmail.com>
2012-05-24 22:35:49 +0200

    app: fix the reading, the browsing algorithms seems ok
        * Makefile: add make check1 as a temporary diff check
        * app.cpp: fix the browsing algorithm

--- src/app.cpp
+++ src/app.cpp
@@ -13,14 +13,18 @@ using namespace mitsake;
 
 void node_call(TrieNode* node, std::string previous) {
 
-    for (unsigned int i = 0; i < node->child_count; i++) {
-        TrieLink* link = (TrieLink*)(&(node[1]));
+    if (node->frequency > 0) {
+        std::cout << previous << "\t" << node->frequency << std::endl;
+    }
 
-        std::cout << previous << " -> " << link->l1.letter << std::endl;
+    for (unsigned int i = 0; i < node->child_count; i++) {
+        TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
+
+        //std::cout << previous << " -> " << link->l1.letter << ", offset: " << link->offset << std::endl;
         // TODO: iterate through compressed links
 
         long offset = link->offset;
-        TrieNode* son = (TrieNode*)&(node[-offset/4]);
+        TrieNode* son = (TrieNode*)&(node[-offset/sizeof(TrieNode)]);
 
         std::string next(previous);
         next.push_back(link->l1.letter);
@@ -31,11 +35,11 @@ void node_call(TrieNode* node, std::string previous) {
 void recursive_descent(unsigned int* mapped) {
     unsigned int offset = mapped[0] / 4;
 
-    std::cout << "offset: " << mapped[0] << std::endl;
+    //std::cout << "offset: " << mapped[0] << std::endl;
 
     TrieNode* root = (TrieNode*)(&mapped[offset]);
 
-    std::cout << "root-freq: " << root->frequency << ", root-childs: " << root->child_count << std::endl;
+    //std::cout << "root-freq: " << root->frequency << ", root-childs: " << root->child_count << std::endl;
 
     node_call(root, "");
 }
@@ -47,10 +51,6 @@ int main(int argc, char** argv) {
 
     int fd = open(argv[1], O_RDONLY);
     unsigned int* mapped = (unsigned int*)mmap(0, fs.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-
-    std::cout << sizeof(TrieNode) << std::endl;
-    std::cout << sizeof(TrieLink) << std::endl;
-    std::cout << sizeof(unsigned int) << std::endl;
 
     recursive_descent(mapped);
 
