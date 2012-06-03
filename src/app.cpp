a5f296bdaf9c05e8923c952c5e229b7b98767822
kutsuru <kutsuru@gmail.com>
2012-06-03 13:19:41 +0200

    App: Added distance of leveinshtein for Trie, to be improved.

--- src/app.cpp
+++ src/app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <algorithm>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -9,7 +10,16 @@
 #include "trie-dummy.hpp"
 #include "trie-fast.hpp"
 
+#define DEBUG_DIST 1
+#define DEBUG_DESCENT 0
+#define DEBUG_DEL 0
+#define DEBUG_SUB 0
+#define DEBUG_INS 1
+
 using namespace mitsake;
+
+
+static std::map<std::string, unsigned int> suggestion;
 
 void node_call(TrieNode* node, std::string previous) {
 
@@ -20,11 +30,14 @@ void node_call(TrieNode* node, std::string previous) {
     for (unsigned int i = 0; i < node->child_count; i++) {
         TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
 
-        //std::cout << previous << " -> " << link->l1.letter << ", offset: " << link->offset << std::endl;
-        // TODO: iterate through compressed links
+#if DEBUG_DESCENT
+        std::cout << previous << " -> " << link->l1.letter << ", offset: "
+            << link->offset << std::endl;
+#endif
 
+        // TODO: iterate through compressed links
         long offset = link->offset;
-        TrieNode* son = (TrieNode*)&(node[-offset/sizeof(TrieNode)]);
+        TrieNode* son = (TrieNode*)&(node[-offset / sizeof (TrieNode)]);
 
         std::string next(previous);
         next.push_back(link->l1.letter);
@@ -32,27 +45,182 @@ void node_call(TrieNode* node, std::string previous) {
     }
 }
 
-void recursive_descent(unsigned int* mapped) {
+TrieNode* recursive_descent(unsigned int* mapped) {
     unsigned int offset = mapped[0] / 4;
 
-    //std::cout << "offset: " << mapped[0] << std::endl;
+#if DEBUG_DESCENT
+    std::cout << "offset: " << mapped[0] << std::endl;
+#endif
 
     TrieNode* root = (TrieNode*)(&mapped[offset]);
 
-    //std::cout << "root-freq: " << root->frequency << ", root-childs: " << root->child_count << std::endl;
+#if DEBUG_DESCENT
+    std::cout << "root-freq: " << root->frequency << ", root-childs: "
+        << root->child_count << std::endl;
+#endif
 
     node_call(root, "");
+    return root;
 }
+
+int compute_distance(TrieNode* node, std::string previous, std::string word,
+        int index, int length, int distance, int treshold)
+{
+    if (distance > treshold)
+        return distance;
+
+    int result = -1;
+    int mdistance = -1;
+
+#if DEBUG_DIST
+    std::cout << "Current Node -> " << previous << " : "
+        << node->child_count << " children, dist = " << distance << std::endl;
+#endif
+
+
+    if (node->frequency != 0)
+    {
+#if DEBUG_DIST
+        std::cout << "Working word : " << previous << std::endl;
+#endif
+
+        suggestion.insert(std::pair<std::string, unsigned int>
+                (previous, node->frequency));
+        result = length;
+    }
+
+    if (distance + 1 <= treshold)
+    {
+        // Deletion step
+        int deletion = compute_distance(node, previous, word, index + 1,
+                length, distance + 1, treshold);
+        result = std::min(result, deletion);
+
+#if DEBUG_DIST && DEBUG_DEL      
+        std::cout << "Deletion step ==> del : " << deletion
+            << " , result : " << result << std::endl;
+#endif
+
+        for (unsigned int i = 0; i < node->child_count; i++)
+        {
+            TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
+
+            long offset = link->offset;
+            TrieNode* son = (TrieNode*)&(node[-offset / sizeof (TrieNode)]);
+
+            std::string next(previous);
+            next.push_back(link->l1.letter);
+
+#if DEBUG_DIST && DEBUG_SUB
+            std::cout << "Length : " << length
+                << ", Index : " << index
+                << ", Current label : " << next[index]
+                << ", Current character : " << word[index]
+                << std::endl;
+#endif
+
+            //Substitution step
+            if (length > index + 1 && link->l1.letter == word[index])
+                mdistance = 0;
+            else
+                mdistance = 1;
+
+            int substitution = compute_distance(son, next, word, index + 1,
+                    length, distance + mdistance,
+                    treshold);
+            result = std::min(result, substitution);
+
+#if DEBUG_DIST && DEBUG_SUB      
+            std::cout << "Substitution step ==> sub : " << substitution
+                << " , result : " << result << std::endl;
+#endif
+
+            // Insertion step
+            int insertion = compute_distance(son, next, word, index,
+                    length, distance + 1, treshold);
+
+            result = std::min(result, insertion);
+
+            // Transposition step
+
+
+#if DEBUG_DIST && DEBUG_INS
+            std::cout << "Insertion step ==> ins : " << insertion
+                << " , result : " << result << std::endl;
+#endif
+        }
+    }
+
+    return result;
+}
+
+void test(std::pair<std::string, unsigned int>* p)
+{
+    std::cout << "(" << p->first << "," << p->second << ")" << std::endl;
+}
+
 
 int main(int argc, char** argv) {
 
-    struct stat fs;
-    stat(argv[1], &fs);
+    if (argc > 1)
+    {
+        struct stat fs;
+        stat(argv[1], &fs);
 
-    int fd = open(argv[1], O_RDONLY);
-    unsigned int* mapped = (unsigned int*)mmap(0, fs.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        int fd = open(argv[1], O_RDONLY);
+        if (fd > -1)
+        {
+            int treshold = 0;
+            std::string word;
+            std::string command;
 
-    recursive_descent(mapped);
+            unsigned int* mapped =
+                (unsigned int*) mmap(0, fs.st_size, PROT_READ, MAP_PRIVATE,
+                        fd, 0);
+
+            TrieNode* root = recursive_descent(mapped);
+
+            // Avoid overhead on I/O
+            std::cin.sync_with_stdio(false);
+
+            while (std::cin >> command >> treshold >> word)
+            {
+                if (command.compare("quit") == 0)
+                    break; 
+
+                std::cout << "Command : " << command << std::endl;
+                std::cout << "Treshold: " << treshold << std::endl;
+                std::cout << "Word : " << word << std::endl;
+
+                if (command.compare("approx") == 0)
+                    std::cout << "Result : "
+                        << compute_distance(root, "", word, 0,
+                                word.length(), 0,
+                                treshold)
+                        << std::endl;
+
+                else
+                    std::cout << "Unknown command, you can only use approx"
+                        << std::endl;
+
+#if 0
+                std::for_each (suggestion.begin(), suggestion.end(),
+                        [](typename std::map<std::string, unsigned int>::iterator it)
+                        {
+                        std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
+                        });
+#endif
+
+                for (std::map<std::string, unsigned int>::iterator it = suggestion.begin( );
+                     it != suggestion.end( ) ; ++it)
+                    std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
+            }
+        }
+        else
+            std::cout << "Invalid file" << std::endl;
+    }
+    else
+        std::cout << "Usage: ./app words.dic" << std::endl;
 
     return 0;
 }
