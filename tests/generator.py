81642eafeec32ab9e129cfa5c0af97d1dc0f3ea2
Laurent Senta <laurent.senta@gmail.com>
2012-06-12 12:10:42 +0200

    test: generator also create the chapter_ structure

--- tests/generator.py
+++ tests/generator.py
@@ -3,6 +3,10 @@
 import sys
 import subprocess
 import random
+import os
+
+sys.path.append("lib")
+import yaml
 
 def mixup_word(word):
     for i in range(random.randint(0, 5)):
@@ -11,6 +15,7 @@ def mixup_word(word):
             ])(word)
 
     return word
+
 
 def gen_test(words):
     r = []
@@ -27,22 +32,54 @@ def main(args):
     comp_refp = "./TextMiningCompiler"
     comp_usp = "../compiler"
 
+    app_refp = "../../TextMiningApp"
+    app_usp = "../../app"
+
     for path in files:
+        prefix = ".".join(path.split(".")[:-1])
+        folder = "chapter_%s" % prefix
+
+        try:
+            os.mkdir(folder)
+        except Exception as e:
+            print "mkdir %s failed, exception: %s" % (folder, e)
+
+        path_tst = os.path.join(folder, prefix + ".tst")
+        path_ref = os.path.join(folder, prefix + ".ref")
+        path_us = os.path.join(folder, prefix + ".us")
+        path_yml = os.path.join(folder, prefix + ".yml")
+
         with open(path, "r") as f:
             lines = f.readlines()
 
         print "compiling [ref]: ", path
-        subprocess.call([comp_refp, path, path + ".ref"])
+        subprocess.call([comp_refp, path, path_ref])
 
         # TODO: lock on -c with our compiler!
         print "compiling [us]: ", path
-        subprocess.call([comp_usp, path, path + ".us"])
+        subprocess.call([comp_usp, path, path_us])
 
         print "test generation"
         words = map(lambda x: x.split()[0], lines)
         lines = gen_test(words)
-        with open(path + ".tst", "w") as f:
+        with open(path_tst, "w") as f:
             f.writelines(lines)
+
+        print "gen yaml"
+
+        d = {
+            "ref": "cat %s | %s %s 2>/dev/null" % (prefix + ".tst", app_refp, prefix + ".ref"),
+            "command": "cat %s | %s %s 2>/dev/null" % (prefix + ".tst", app_usp, prefix + ".us"),
+            "stdout": "",
+            "stderr": "",
+            "retval": "",
+            "info": "",
+        }
+        print dir(yaml)
+
+        with open(path_yml, "w") as f:
+            yaml.dump(d, f)
+
 
 if __name__ == "__main__":
     main(sys.argv)
