e39694d533a879beb8aabbc917100231f5febcc7
kutsuru <kutsuru@gmail.com>
2012-06-14 23:26:04 +0200

    App: Complete ld distance, now optitime !

--- src/app.cpp
+++ src/app.cpp
@@ -16,20 +16,11 @@ using namespace mitsake;
 
 static std::set<SortStruct> suggestions;
 
-void node_call(TrieNode* node, std::string previous) {
-
-    if (node->frequency > 0)
-#if DEBUG_DESCENT
-        std::cout << previous << "\t" << node->frequency << std::endl;
-#endif
-
-    for (unsigned int i = 0; i < node->child_count; i++) {
+void node_call(TrieNode* node, std::string previous)
+{
+    for (unsigned int i = 0; i < node->child_count; i++)
+    {
         TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
-
-#if DEBUG_DESCENT
-        std::cout << previous << " -> " << link->l1.letter << ", offset: "
-            << link->offset << std::endl;
-#endif
 
         // TODO: iterate through compressed links
         long offset = link->offset;
@@ -41,86 +32,76 @@ void node_call(TrieNode* node, std::string previous) {
     }
 }
 
-TrieNode* recursive_descent(unsigned int* mapped) {
+TrieNode* recursive_descent(unsigned int* mapped)
+{
     unsigned int offset = mapped[0] / 4;
-
-#if DEBUG_DESCENT
-    std::cout << "offset: " << mapped[0] << std::endl;
-#endif
-
     TrieNode* root = (TrieNode*)(&mapped[offset]);
-
-#if DEBUG_DESCENT
-    std::cout << "root-freq: " << root->frequency << ", root-childs: "
-        << root->child_count << std::endl;
-#endif
 
     node_call(root, "");
     return root;
 }
 
-unsigned int compute_distance(TrieNode* node, std::string previous, std::string word,
-        int index, int length, unsigned short distance, int treshold)
+void compute_distance(TrieNode* node, std::string previous, std::string word,
+                      int index, int length, unsigned short distance,
+                      int threshold)
 {
-    if (distance > treshold)
-        return distance;
-
-    unsigned int result = -1;
-    int mdistance = -1;
+    if (distance > threshold)
+        return;
 
     if (node->frequency != 0 &&
-        distance + length - index <= treshold &&
+        distance + length - index <= threshold &&
         index <= length)
     {
         SortStruct triple(distance + length - index, node->frequency, previous);
         suggestions.insert(triple);
-        result = distance + length - index;
     }
-    if  (distance <= treshold)
+
+    // Deletion step
+    compute_distance(node, previous, word, index + 1, length, distance + 1,
+                     threshold);
+
+    for (unsigned int i = 0; i < node->child_count; i++)
     {
-        // Deletion step
-        unsigned int deletion = compute_distance(node, previous, word, index + 1,
-                length, distance + 1, treshold);
-        result = std::min(result, deletion);
+        TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
+        long offset = link->offset;
+        TrieNode* son = (TrieNode*)&(node[-offset / sizeof (TrieNode)]);
 
-        for (unsigned int i = 0; i < node->child_count; i++)
+        std::string next(previous);
+        next.push_back(link->l1.letter);
+
+        if (index < length)
         {
-            TrieLink* link = &(((TrieLink*)(&(node[1])))[i]);
-            long offset = link->offset;
-            TrieNode* son = (TrieNode*)&(node[-offset / sizeof (TrieNode)]);
+            int mdistance = -1;
 
-            std::string next(previous);
-            next.push_back(link->l1.letter);
+            //Substitution step
+            if (length > index && (link->l1.letter == (127 & word[index])))
+                mdistance = 0;
+            else
+                mdistance = 1;
 
-            if (index < length)
+            compute_distance(son, next, word, index + 1,
+                    length, distance + mdistance,
+                    threshold);
+        }
+
+        // Insertion
+        compute_distance(son, next, word, index, length, distance + 1,
+                         threshold);
+
+        // Transposition step
+        if (index + 1 < length)
+        {
+            for (unsigned int i = 0; i < son->child_count; i++)
             {
-                //Substitution step
-                if (length > index && (link->l1.letter == (127 & word[index])))
-                    mdistance = 0;
-                else
-                    mdistance = 1;
+                TrieLink* son_link = &(((TrieLink*)(&(son[1])))[i]);
 
-                compute_distance(son, next, word, index + 1,
-                        length, distance + mdistance,
-                        treshold);
-            }
-
-            // Insertion
-            compute_distance(son, next, word, index,
-                    length, distance + 1, treshold);
-
-            // Transposition step
-            if (index + 1 < length)
-            {
-                for (unsigned int i = 0; i < son->child_count; i++)
+                if (link->l1.letter == (127 & word[index + 1]))
                 {
-                    TrieLink* son_link = &(((TrieLink*)(&(son[1])))[i]);
-
-                    if (link->l1.letter == (127 & word[index + 1]) &&
-                        son_link->l1.letter == (127 & word[index]))
+                    if (son_link->l1.letter == (127 & word[index]))
                     {
                         long offset = son_link->offset;
-                        TrieNode* grandson = (TrieNode*)&(son[-offset / sizeof (TrieNode)]);
+                        TrieNode* grandson =
+                            (TrieNode*)&(son[-offset / sizeof (TrieNode)]);
 
                         std::string next_next(next);
                         next_next.push_back(son_link->l1.letter);
@@ -128,23 +109,21 @@ unsigned int compute_distance(TrieNode* node, std::string previous, std::string 
                         if (grandson)
                             compute_distance(grandson, next_next, word,
                                              index + 2, length, distance + 1,
-                                             treshold);
+                                             threshold);
                     }
-                    else if (link->l1.letter == (127 & word[index + 1]))
+                    else
                     {
                         std::string word_modified = word;
                         char l = word_modified[index + 1];
                         word_modified[index + 1] = index;
                         word_modified[index] = l;
-                        compute_distance(son, next, word_modified, index + 1, length, distance + 1, treshold);
+                        compute_distance(son, next, word_modified, index + 1,
+                                         length, distance + 1, threshold);
                     }
-
                 }
             }
         }
     }
-
-    return result;
 }
 
 void erase_duplicata()
@@ -153,21 +132,8 @@ void erase_duplicata()
 
     for (std::set<SortStruct>::iterator it = suggestions.begin();
             it != suggestions.end( ) ; ++it)
-    {
         if (!(unique_result.insert(it->word)).second)
-        {
-#if DEBUG_DIST
-            std::cout << "TRY-1 Erasing word : " << it->word
-                      << ", distance : " << it->distance << std::endl;
-#endif
             suggestions.erase(it--);
-        }
-#if DEBUG_DIST
-        else
-            std::cout << "TRY-1 Keeping word : " << it->word
-                      << ", distance : " << it->distance << std::endl;
-#endif
-    }
 }
 
 void suggestion_display(std::set<SortStruct> &suggestions) {
@@ -212,7 +178,7 @@ int main(int argc, char** argv) {
         return 2;
     }
 
-    int treshold = 0;
+    int threshold = 0;
     std::string word;
     std::string command;
 
@@ -225,16 +191,10 @@ int main(int argc, char** argv) {
     // Avoid overhead on I/O
     std::cin.sync_with_stdio(false);
 
-    while (std::cin >> command >> treshold >> word)
+    while (std::cin >> command >> threshold >> word)
     {
         if (command.compare("quit") == 0)
             break;
-
-#if DEBUG_DIST
-        std::cout << "Command : " << command << std::endl;
-        std::cout << "Treshold: " << treshold << std::endl;
-        std::cout << "Word : " << word << std::endl;
-#endif
 
         if (command.compare("approx") != 0)
             std::cout << "Unknown command, you can only use approx"
@@ -243,7 +203,7 @@ int main(int argc, char** argv) {
         {
             suggestions.clear();
             compute_distance(root, "", word, 0, word.length(), 0,
-                             treshold);
+                             threshold);
             suggestion_display(suggestions);
         }
     }
