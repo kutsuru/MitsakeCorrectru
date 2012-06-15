82df3ca11d6dbdc38ae0a8980f0e533294bc2926
kutsuru <kutsuru@gmail.com>
2012-06-15 11:31:22 +0200

    Tbb: testing phase.

--- src/app.cpp
+++ src/app.cpp
@@ -8,13 +8,16 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#include <tbb/parallel_invoke.h>
+#include <tbb/concurrent_vector.h>
+
 #include "trie-dummy.hpp"
 #include "trie-fast.hpp"
 #include "sort-struct.hpp"
 
 using namespace mitsake;
 
-static std::set<SortStruct> suggestions;
+static tbb::concurrent_vector<SortStruct> vector;
 
 void node_call(TrieNode* node, std::string previous)
 {
@@ -53,7 +56,8 @@ void compute_distance(TrieNode* node, std::string previous, std::string word,
         index <= length)
     {
         SortStruct triple(distance + length - index, node->frequency, previous);
-        suggestions.insert(triple);
+        vector.push_back(triple);
+        //suggestions.insert(triple);
     }
 
     // Deletion step
@@ -69,60 +73,68 @@ void compute_distance(TrieNode* node, std::string previous, std::string word,
         std::string next(previous);
         next.push_back(link->l1.letter);
 
+        tbb::parallel_invoke(
+        [=]()
+        {
         if (index < length)
         {
-            int mdistance = -1;
+        int mdistance = -1;
 
-            //Substitution step
-            if (length > index && (link->l1.letter == (127 & word[index])))
-                mdistance = 0;
-            else
-                mdistance = 1;
+        //Substitution step
+        if (length > index && (link->l1.letter == (127 & word[index])))
+        mdistance = 0;
+        else
+        mdistance = 1;
 
-            compute_distance(son, next, word, index + 1,
-                    length, distance + mdistance,
-                    threshold);
+        compute_distance(son, next, word, index + 1,
+            length, distance + mdistance, threshold);
         }
-
+        },
+        [=]()
+        {
         // Insertion
         compute_distance(son, next, word, index, length, distance + 1,
-                         threshold);
-
-        // Transposition step
-        if (index + 1 < length)
+            threshold);
+        },
+        [=]()
         {
-            for (unsigned int i = 0; i < son->child_count; i++)
+            // Transposition step
+            if (index + 1 < length)
             {
-                TrieLink* son_link = &(((TrieLink*)(&(son[1])))[i]);
-
-                if (link->l1.letter == (127 & word[index + 1]))
+                for (unsigned int i = 0; i < son->child_count; i++)
                 {
-                    if (son_link->l1.letter == (127 & word[index]))
-                    {
-                        long offset = son_link->offset;
-                        TrieNode* grandson =
-                            (TrieNode*)&(son[-offset / sizeof (TrieNode)]);
+                    TrieLink* son_link = &(((TrieLink*)(&(son[1])))[i]);
 
-                        std::string next_next(next);
-                        next_next.push_back(son_link->l1.letter);
-
-                        if (grandson)
-                            compute_distance(grandson, next_next, word,
-                                             index + 2, length, distance + 1,
-                                             threshold);
-                    }
-                    else
+                    if (link->l1.letter == (127 & word[index + 1]))
                     {
-                        std::string word_modified = word;
-                        char l = word_modified[index + 1];
-                        word_modified[index + 1] = index;
-                        word_modified[index] = l;
-                        compute_distance(son, next, word_modified, index + 1,
-                                         length, distance + 1, threshold);
+                        if (son_link->l1.letter == (127 & word[index]))
+                        {
+                            long offset = son_link->offset;
+                            TrieNode* grandson =
+                                (TrieNode*)&(son[-offset / sizeof (TrieNode)]);
+
+                            std::string next_next(next);
+                            next_next.push_back(son_link->l1.letter);
+
+                            if (grandson)
+                                compute_distance(grandson, next_next, word,
+                                        index + 2, length, distance + 1,
+                                        threshold);
+                        }
+                        else
+                        {
+                            std::string word_modified = word;
+                            char l = word_modified[index + 1];
+                            word_modified[index + 1] = index;
+                            word_modified[index] = l;
+                            compute_distance(son, next, word_modified, index + 1,
+                                    length, distance + 1, threshold
+                                    );
+                        }
                     }
                 }
             }
-        }
+        });
     }
 }
 
@@ -136,7 +148,8 @@ void erase_duplicata()
             suggestions.erase(it--);
 }
 
-void suggestion_display(std::set<SortStruct> &suggestions) {
+void suggestion_display(std::set<SortStruct>& suggestions)
+{
     if (suggestions.size() == 0)
     {
         std::cout << "[" << "]" << std::endl;
@@ -160,8 +173,43 @@ void suggestion_display(std::set<SortStruct> &suggestions) {
     std::cout << "]" << std::endl;
 }
 
-int main(int argc, char** argv) {
+void damerau_levenshtein(TrieNode* root)
+{
+    std::set<SortStruct> suggestions;
 
+    // Avoid overhead on I/O
+    std::cin.sync_with_stdio(false);
+
+    int threshold = 0;
+    std::string word;
+    std::string command;
+
+    while (std::cin >> command >> threshold >> word)
+    {
+        if (command.compare("quit") == 0)
+            break;
+
+        if (command.compare("approx") != 0)
+            std::cout << "Unknown command, you can only use approx"
+                << std::endl;
+        else
+        {
+            suggestions.clear();
+            vector.clear();
+            compute_distance(root, "", word, 0, word.length(), 0,
+                             threshold);
+
+            for (auto sort_struct : vector)
+                suggestions.insert(sort_struct);
+
+
+            suggestion_display(suggestions);
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
     if (argc < 2)
     {
         std::cout << "Usage: ./app words.dic" << std::endl;
@@ -178,35 +226,13 @@ int main(int argc, char** argv) {
         return 2;
     }
 
-    int threshold = 0;
-    std::string word;
-    std::string command;
-
     unsigned int* mapped = (unsigned int*) mmap(0, fs.st_size,
                                                 PROT_READ, MAP_PRIVATE,
                                                 fd, 0);
 
     TrieNode* root = recursive_descent(mapped);
 
-    // Avoid overhead on I/O
-    std::cin.sync_with_stdio(false);
-
-    while (std::cin >> command >> threshold >> word)
-    {
-        if (command.compare("quit") == 0)
-            break;
-
-        if (command.compare("approx") != 0)
-            std::cout << "Unknown command, you can only use approx"
-                << std::endl;
-        else
-        {
-            suggestions.clear();
-            compute_distance(root, "", word, 0, word.length(), 0,
-                             threshold);
-            suggestion_display(suggestions);
-        }
-    }
+    damerau_levenshtein(root);
 
     return 0;
 }
